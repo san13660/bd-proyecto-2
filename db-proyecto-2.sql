7948f62ede0a89b98e16867f91cfc1c13b3683ad
Christopher Sandoval <san13660@uvg.edu.gt>
2019-05-16 15:09:15 -0600

    cambio de tamaños varchar

--- db-proyecto-2.sql
+++ db-proyecto-2.sql
@@ -1,6 +1,6 @@
 CREATE TABLE cliente (
 	nit VARCHAR(10) PRIMARY KEY,
-	nombre VARCHAR(30));
+	nombre VARCHAR(50));
 	
 
 CREATE TABLE factura (
@@ -11,7 +11,7 @@ CREATE TABLE factura (
 
 CREATE TABLE producto (
 	id VARCHAR(10) PRIMARY KEY,
-	nombre VARCHAR(20),
+	nombre VARCHAR(50),
 	categoria VARCHAR(20),
 	marca VARCHAR(20),
 	precio REAL);
@@ -30,8 +30,8 @@ CREATE TABLE tipo_custom (
 	
 
 CREATE TABLE custom (
-	id_producto VARCHAR(20) REFERENCES producto(id),
-	nombre_tipo_custom VARCHAR(20) REFERENCES tipo_custom(nombre),
+	id_producto VARCHAR(10) REFERENCES producto(id),
+	nombre_tipo_custom VARCHAR(30) REFERENCES tipo_custom(nombre),
 	valor VARCHAR(50),
 	PRIMARY KEY (id_producto, nombre_tipo_custom));
 	
@@ -43,11 +43,11 @@ INSERT INTO cliente(nit, nombre) VALUES('55661854', 'Florentina Gonzales');
 INSERT INTO cliente(nit, nombre) VALUES('57484886', 'Raul Gonzales');
 INSERT INTO cliente(nit, nombre) VALUES('88484556', 'Eduardo Gonzales');
 
-INSERT INTO factura(id, nit, fecha, total) VALUES('165184', '25685987', '2019-05-15 11:45:05', 3800.99);
+INSERT INTO factura(nit, fecha, total) VALUES('25685987', '2019-05-15 11:45:05', 3800.99);
 INSERT INTO producto(id, nombre, categoria, marca, precio) VALUES('1561851', 'Lavadora', 'Linea Blanca', 'LG', 3000.00);
 INSERT INTO producto(id, nombre, categoria, marca, precio) VALUES('1561895', 'Microondas', 'Linea Blanca', 'Whirlpool', 800.99);
-INSERT INTO linea_factura(id, id_factura, id_producto, cantidad, sub_total) VALUES('551651', '165184', '1561851', 1, 3000.00);
-INSERT INTO linea_factura(id, id_factura, id_producto, cantidad, sub_total) VALUES('551652', '165184', '1561895', 1, 800.99);
+INSERT INTO linea_factura(id_factura, id_producto, cantidad, sub_total) VALUES('165184', '1561851', 1, 3000.00);
+INSERT INTO linea_factura(id_factura, id_producto, cantidad, sub_total) VALUES('165184', '1561895', 1, 800.99);
 
 INSERT INTO tipo_custom(nombre, tipo) VALUES('color', 'string');
 INSERT INTO tipo_custom(nombre, tipo) VALUES('peso', 'float');
